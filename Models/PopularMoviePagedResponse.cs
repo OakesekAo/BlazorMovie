c9fb9c015ec3f807699e66b54c4ae4bbe0289091
OakeseskAo <oakes.a@live.com>
2023-11-22 01:45:30 -0500

    Started to setup api call
    Added api key retreival
    stubbed out pop movie class

---
+++ Models/PopularMoviePagedResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMovie.Models
+{
+    public class PopularMoviePagedResponse
+    {
+        public int page { get; set; }
+        public PopularMovie[] results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+
+
+    }
+}
