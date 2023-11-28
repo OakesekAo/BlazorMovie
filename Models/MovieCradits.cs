858e53511bbe3c224d82a130d1babbc987f9d93b
OakeseskAo <oakes.a@live.com>
2023-11-27 23:38:42 -0500

    Session6.5
    Worked through background post img pathing issue

--- Models/MovieCradits.cs
+++ Models/MovieCradits.cs
@@ -1,6 +1,93 @@
-﻿namespace BlazorMovie.Models
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorMovie.Models
 {
-    public class MovieCradits
+    public class MovieCredits
     {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("cast")]
+        public Cast[] Cast { get; set; } = [];
+
+        [JsonPropertyName("crew")]
+        public Crew[] Crew { get; set; } = [];
     }
+
+    public class Cast
+    {
+        [JsonPropertyName("adult")]
+        public bool Adult { get; set; }
+
+        [JsonPropertyName("gender")]
+        public int Gender { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("known_for_department")]
+        public string? KnownForDepartment { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("original_name")]
+        public string? OriginalName { get; set; }
+
+        [JsonPropertyName("popularity")]
+        public float Popularity { get; set; }
+
+        [JsonPropertyName("profile_path")]
+        public string? ProfilePath { get; set; }
+
+        [JsonPropertyName("cast_id")]
+        public int CastId { get; set; }
+
+        [JsonPropertyName("character")]
+        public string? Character { get; set; }
+
+        [JsonPropertyName("credit_id")]
+        public string? CreditId { get; set; }
+
+        [JsonPropertyName("order")]
+        public int Order { get; set; }
+    }
+
+    public class Crew
+    {
+        [JsonPropertyName("adult")]
+        public bool Adult { get; set; }
+
+        [JsonPropertyName("gender")]
+        public int Gender { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("known_for_department")]
+        public string? KnownForDepartment { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("original_name")]
+        public string? OriginalName { get; set; }
+
+        [JsonPropertyName("popularity")]
+        public float Popularity { get; set; }
+
+        [JsonPropertyName("profile_path")]
+        public string? ProfilePath { get; set; }
+
+        [JsonPropertyName("credit_id")]
+        public string? CreditId { get; set; }
+
+        [JsonPropertyName("department")]
+        public string? Department { get; set; }
+
+        [JsonPropertyName("job")]
+        public string? Job { get; set; }
+    }
+
+
 }
